fd375760f705d1d991dcf64d959862d0b33ac9da
slouchart <sebastien.louchart.itp@gmail.com>
2019-02-06 17:47:22 +0100

    Refactoring inner d.a.g. in Workflow

--- src/gibbon/workflows/base.py
+++ src/gibbon/workflows/base.py
@@ -4,47 +4,16 @@ import logging
 import re
 
 
-from ._dag_utils import _DirectedAcyclicGraph
-
-
-def check_valid_name(name):
-    regexp = re.compile(r'^[a-zA-Z_][a-zA-Z0-9_]*$')
-    return regexp.match(name)
-
-
-class Workflow:
-
-    def __init__(self, name=None):
-        self._dag = _DirectedAcyclicGraph()
+class DirectedAcyclicGraph:
+    def __init__(self):
         self._nodes = dict()
         self._roots = []
-        self._warnings = []
-        self._errors = []
-        self._checked = False
-        self._valid = False
-        self.name = name if name else 'unamed'
-        if not check_valid_name(self.name):
-            self._add_error(InvalidNameError, f'Workflow name is invalid: {self.name}')
 
-    def _create_node(self, parent, cls, *args, **kwargs):
-        node = None
-        try:
-            node = cls(*args, **kwargs)
-        except BaseBuildWarning as w:
-            self._add_warning(w)
-        except BaseBuildError as e:
-            self._add_error(e)
-        except Exception as e:
-            logging.critical(f'Build process for {self.name} has encountered an unexpected exception: {repr(e)}')
+    def create_node(self, parent, cls, *args, **kwargs):
+        node = cls(*args, **kwargs)
 
         if node is None:
             return None
-
-        if not check_valid_name(node.name):
-            self._add_error(InvalidNameError, f'Node name is invalid: {node.name}')
-
-        if self._exists(node):
-            self._add_error(NodeAlreadyExist, f'Duplicate name {node}')
 
         if parent:
             node.set_source(parent)
@@ -54,20 +23,26 @@ class Workflow:
 
         self._nodes[node.id] = node
 
-        self._checked = False
-
         return node
 
-    def _get_node_by_name(self, name):
-        if name not in self._nodes:
-            self._add_error(NodeNotFound, f"Node {name} not found")
-            return None
-        return self._nodes[name]
+    def __getitem__(self, item):
+        return self._nodes[item]
 
-    def _exists(self, node):
-        return node.id in self._nodes
+    def __contains__(self, item):
+        return item in self._nodes
 
-    def _check_reachability(self):
+    @property
+    def nodes(self):
+        return self._nodes.values()
+
+    def is_root(self, node):
+        return node in self._roots
+
+    @property
+    def roots(self):
+        return iter(self._roots)
+
+    def check_reachability(self, callback):
         """reachability invariant #1: there must be at least one path connecting any root source to a target
            reachability invariant #2: there must be at least one path connecting any target to a root source"""
 
@@ -76,8 +51,8 @@ class Workflow:
             if node.has_source:
                 for source in node.sources:
                     _check_path_to_source(source)
-            elif node not in self._roots:
-                self._add_error(NodeReachabilityError, f'Source {node.name} unconnected')
+            elif not self.is_root(node):
+                callback(NodeReachabilityError, f'Source {node.name} unconnected')
             else:
                 pass
 
@@ -88,14 +63,69 @@ class Workflow:
             elif isinstance(node, Target):
                 pass
             else:
-                self._add_error(NodeReachabilityError, f"Transformation {node.name} unconnected to a target")
+                callback(NodeReachabilityError, f"Transformation {node.name} unconnected to a target")
 
-        targets = [n for n in self._nodes.values() if n.has_target]
+        targets = [n for n in self.nodes if n.has_target]
         for target in targets:
             _check_path_to_source(target)
 
-        for source in self._roots:
+        for source in self.roots:
             _check_path_to_target(source)
+
+    def bfs_traverse_links(self, callback):
+        queue = []
+        visited = set()
+        for n in self.roots:
+            queue.append(n)
+        while len(queue):
+            node = queue.pop(0)
+            if node in visited:
+                continue
+            else:
+                visited.add(node)
+                if node.has_target:
+                    for c in node.targets:
+                        callback(node, c)
+                        queue.append(c)
+
+    def bfs_traverse(self, callback):
+        queue = []
+        visited = set()
+        for n in self.roots:
+            queue.append(n)
+        while len(queue):
+            node = queue.pop(0)
+            if node in visited:
+                continue
+            else:
+                callback(node)
+                visited.add(node)
+                if node.has_target:
+                    for c in node.targets:
+                        queue.append(c)
+
+
+class Workflow:
+
+    def __init__(self, name=None):
+        self._dag = DirectedAcyclicGraph()
+        self._warnings = []
+        self._errors = []
+        self._checked = False
+        self._valid = False
+        self.name = name if name else 'unamed'
+        self.check_valid_name(self.name)
+
+    def check_valid_name(self, name):
+        regexp = re.compile(r'^[a-zA-Z_][a-zA-Z0-9_]*$')
+        if not regexp.match(name):
+            self._add_error(InvalidNameError, f'Workflow name is invalid: {name}')
+
+    def get_node_by_name(self, name):
+        if name not in self._dag:
+            self._add_error(NodeNotFound, f"Node {name} not found")
+            return None
+        return self._dag[name]
 
     def _add_error(self, exc, msg=None):
         self._errors.append(exc(msg))
@@ -105,47 +135,52 @@ class Workflow:
 
     def add_source(self, name):
         self._checked = False
-        self._create_node(None, Source, name)
+        self.check_valid_name(name)
+        self._dag.create_node(None, Source, name)
 
     def add_target(self, name, source):
         self._checked = False
-        parent = self._get_node_by_name(source)
-        self._create_node(parent, Target, name)
+        self.check_valid_name(name)
+        parent = self.get_node_by_name(source)
+        self._dag.create_node(parent, Target, name)
 
     def add_transformation(self, name, type, source, targets=(), **kwargs):
 
         self._checked = False
+        self.check_valid_name(name)
 
         parent = None
         if source:
-            parent = self._get_node_by_name(source)
+            parent = self.get_node_by_name(source)
 
-        node = self._create_node(parent, type, name, **kwargs)
+        node = self._dag.create_node(parent, type, name, **kwargs)
 
         for target in targets:
             if target is None:
                 continue
             try:
-                target = self._get_node_by_name(target)
+                target = self.get_node_by_name(target)
                 target.connect_to_source(node)
             except BaseBuildWarning as w:
                 self._add_warning(w)
 
     def add_complex_transformation(self, name, type, sources, targets=(), **kwargs):
         self._checked = False
+        self.check_valid_name(name)
+
         parents = []
         for source in sources:
-            parent = self._get_node_by_name(source)
+            parent = self.get_node_by_name(source)
             if parent:
                 parents.append(parent)
 
-        node = self._create_node(parents, type, name, **kwargs)
+        node = self._dag.create_node(parents, type, name, **kwargs)
 
         for target in targets:
             if target is None:
                 continue
             try:
-                target = self._get_node_by_name(target)
+                target = self.get_node_by_name(target)
                 target.connect_to_source(node)
             except BaseBuildWarning as w:
                 self._add_warning(w)
@@ -168,7 +203,7 @@ class Workflow:
             self._valid = False
         else:
             # is the reachability predicate true?
-            self._check_reachability()
+            self._dag.check_reachability(self._add_error)
 
             if len(self._errors):
                 self._valid = False
@@ -205,48 +240,19 @@ class Workflow:
 
         return s
 
-    def _bfs_traverse_links(self, callback):
-        queue = []
-        visited = set()
-        for n in self._roots:
-            queue.append(n)
-        while len(queue):
-            node = queue.pop(0)
-            if node in visited:
-                continue
-            else:
-                visited.add(node)
-                if node.has_target:
-                    for c in node.targets:
-                        callback(node, c)
-                        queue.append(c)
-
-    def _bfs_traverse(self, callback):
-        queue = []
-        visited = set()
-        for n in self._roots:
-            queue.append(n)
-        while len(queue):
-            node = queue.pop(0)
-            if node in visited:
-                continue
-            else:
-                callback(node)
-                visited.add(node)
-                if node.has_target:
-                    for c in node.targets:
-                        queue.append(c)
-
     def prepare(self, cfg_visitor):
-        self._bfs_traverse(cfg_visitor.set_configuration)
+        if not self.is_valid:
+            logging.error(f"{self.name}: invalid workflow cannot be configured")
+        else:
+            self._dag.bfs_traverse(cfg_visitor.set_configuration)
 
     def reset(self, cfg_visitor):
-        self._bfs_traverse(cfg_visitor.reset_configuration)
+        self._dag.bfs_traverse(cfg_visitor.reset_configuration)
 
     def run(self, exec_visitor, *args, **kwargs):
         if not self.is_valid:
-            logging.error(f"{self.name}: invalid mapping cannot be run")
+            logging.error(f"{self.name}: invalid workflow cannot be run")
         else:
-            self._bfs_traverse_links(exec_visitor.set_queues)
-            self._bfs_traverse(exec_visitor.create_job_from)
+            self._dag.bfs_traverse_links(exec_visitor.set_queues)
+            self._dag.bfs_traverse(exec_visitor.create_job_from)
             exec_visitor.run(self.name, *args, **kwargs)
