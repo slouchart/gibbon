728b23ad05373840fa857726ba109c3ad5fbc5c9
slouchart <sebastien.louchart.itp@gmail.com>
2019-02-07 15:43:34 +0100

    adding type information helpers

--- tests/test_workflow_base.py
+++ tests/test_workflow_base.py
@@ -5,11 +5,25 @@ import unittest
 class TestCreate(unittest.TestCase):
 
     def setUp(self):
-        self.wk = gibbon.Workflow('unamed')
+        self.name = u'unamed'
+        self.invalid_name = u'0uuuu'
+        self.wk = gibbon.Workflow(self.name)
+
+        self.expected_attrs = ('add_source',
+                               'add_target',
+                               'add_transformation',
+                               'add_complex_transformation',
+                               'prepare', 'schedule', 'run')
 
     def test_create(self):
         self.assertIsNotNone(self.wk)
         self.assertIsInstance(self.wk, gibbon.Workflow)
+        self.assertTrue(hasattr(self.wk, 'name'))
+        self.assertEqual(self.wk.name, self.name)
+
+    def test_iface(self):
+        for attr in self.expected_attrs:
+            self.assertTrue(hasattr(self.wk, attr))
 
     def test_no_name(self):
         with self.assertRaises(gibbon.InvalidNameError) as ctx:
@@ -20,10 +34,11 @@ class TestCreate(unittest.TestCase):
 
     def test_invalid_name(self):
         with self.assertRaises(gibbon.InvalidNameError) as ctx:
-            wk = gibbon.Workflow('0uuuu')
+            wk = gibbon.Workflow(self.invalid_name)
             wk.raise_last()
         exc = ctx.exception
         self.assertRegex(str(exc), r'^Object name is invalid:')
+        self.assertEqual(wk.name, self.invalid_name)
 
 
 if __name__ == '__main__':
